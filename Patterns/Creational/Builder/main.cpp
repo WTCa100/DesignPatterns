28e0d8fa6fc762422fbfa7d30b5722ee2c5be3c1
biejan <biejan@Ubuntu.myguest.virtualbox.org>
2023-09-15 21:27:10 +0200

    Added the final `builder` design pattern

--- Patterns/Creational/Builder/main.cpp
+++ Patterns/Creational/Builder/main.cpp
@@ -11,9 +11,151 @@
 #include <string>
 #include <map>
 
+// Concrete product 1 
+class Phone
+{
+    public:
+    // Essential information
+    std::string brand;
+    std::string model;
+    int ramCount;
+    int cameraPixelCount;
+    int batteryCapacity;
+    // Additional information
+    std::map<std::string, std::string> additionalInfo;
+
+    // Methods
+    void showBasic()
+    {
+        std::cout << "Phone " << brand << " " << " with specs: " << std::endl;
+        std::cout << "RAM: " << ramCount << "GB" << std::endl;
+        std::cout << "Back camera pixel count: " << cameraPixelCount << "MP" << std::endl;
+        std::cout << "Battery cappacity: " << batteryCapacity << "mAh" << std::endl; 
+    }
+    
+    void showExtened()
+    {
+        if(additionalInfo.size() > 0)
+        {
+            std::cout << "Additional phone information: " << std::endl;
+            for(auto entry : additionalInfo)
+            {
+                std::cout << entry.first << ": " << entry.second << std::endl;
+            }
+        }
+    }
+};
+
+// Inteface builder
+class Builder
+{
+    public:
+    virtual void reset() = 0;
+    virtual void setBrand(std::string phoneBrand) = 0;
+    virtual void setModel(std::string phoneModel) = 0;
+    virtual void setRamCount(int count) = 0;
+    virtual void setBackCameraPixelCount(int count) = 0;
+    virtual void setBatteryCapacity(int capacity) = 0;
+    virtual void setWaterproof(bool isWaterproof) = 0;
+    virtual void setHasFrontCamera(bool hasCameraFront) = 0;
+    virtual ~Builder() {}
+};
+
+// Concrete builder
+class PhoneBuilder : public Builder
+{
+    Phone* initPhone;
+    public:
+    void reset() override
+    { this->initPhone = new Phone; }
+    void setBrand(std::string phoneBrand) override
+    { this->initPhone->brand = phoneBrand; }
+    void setModel(std::string phoneModel) override
+    { this->initPhone->model = phoneModel; }
+    void setRamCount(int count) override
+    { this->initPhone->ramCount = count; }
+    void setBackCameraPixelCount(int count) override
+    { this->initPhone->cameraPixelCount = count; }
+    void setBatteryCapacity(int capacity) override
+    { this->initPhone->batteryCapacity = capacity; }
+    void setWaterproof(bool isWaterproof) override
+    { isWaterproof ? this->initPhone->additionalInfo.insert(std::make_pair("Waterproof", "Yes")) : this->initPhone->additionalInfo.insert(std::make_pair("Waterproof", "No")); }
+    void setHasFrontCamera(bool hasCameraFront) override
+    { hasCameraFront ? this->initPhone->additionalInfo.insert(std::make_pair("Camera -front ", "Yes")) : this->initPhone->additionalInfo.insert(std::make_pair("Camera -front ", "No")); }
+    Phone* getProduct()
+    {
+        Phone* productPhone = initPhone;
+        this->reset();
+        return productPhone;
+    }
+
+    ~PhoneBuilder()
+    { if(initPhone) { delete initPhone; } }
+};
+
+class Director
+{
+    private:
+    Builder* concreteCreator;
+    public:
+    void changeBuilder(Builder* newBuilder)
+    { this->concreteCreator = newBuilder; }
+    void createMePhone()
+    {
+        this->concreteCreator->setBrand("Pear");
+        this->concreteCreator->setModel("MePhone 11");
+        this->concreteCreator->setRamCount(8);
+        this->concreteCreator->setBackCameraPixelCount(12);
+        this->concreteCreator->setBatteryCapacity(12500);
+    }
+    void createMePhonePro()
+    {
+        this->concreteCreator->setBrand("Pear");
+        this->concreteCreator->setModel("MePhone 11 Pro");
+        this->concreteCreator->setRamCount(12);
+        this->concreteCreator->setBackCameraPixelCount(16);
+        this->concreteCreator->setBatteryCapacity(12500);
+        this->concreteCreator->setWaterproof(true);
+        this->concreteCreator->setHasFrontCamera(true);
+    }
+};
+
+void clientCode(Director* director)
+{
+    // Decleare builder
+    PhoneBuilder* phoneSpammingMachine = new PhoneBuilder;
+    // Apply it in the Director
+    director->changeBuilder(phoneSpammingMachine);
+    // Build the phone
+    director->createMePhone();
+    Phone* mePhoneNormal = phoneSpammingMachine->getProduct();
+
+    // Interact with the product
+    mePhoneNormal->showBasic();
+    mePhoneNormal->showExtened();
+
+    // Cleanup
+    delete mePhoneNormal;
+
+    // Create another phone - builder is ready to create another instance
+    director->createMePhonePro();
+    Phone* mePhonePro = phoneSpammingMachine->getProduct();
+
+    // Interact again
+    mePhonePro->showBasic();
+    mePhonePro->showExtened();
+
+    // Cleanup
+    delete mePhonePro;
+
+    // Builders are no longer needed
+    delete phoneSpammingMachine;
+}
+
 int main(int argc, char const *argv[])
 {
-    std::cout << __cplusplus << std::endl;
-    std::pair<int, int> test = {1,2};
+    Director* concreteCreator = new Director;
+    clientCode(concreteCreator);
+    delete concreteCreator;
     return 0;
 }
